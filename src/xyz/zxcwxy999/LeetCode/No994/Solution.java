e881eb4361dc8b1259b6b53bd1fe4f2e1c645ffd
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-03-04 23:54:51 +0800

    LeetCode 2020.03.04!!!贵在坚持！！！

---
+++ src/xyz/zxcwxy999/LeetCode/No994/Solution.java
@@ -0,0 +1,51 @@
+package xyz.zxcwxy999.LeetCode.No994;
+
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+class Solution {
+    int[] dr = new int[]{-1, 0, 1, 0};//用来搜索用的
+    int[] dc = new int[]{0, -1, 0, 1};
+
+    public int orangesRotting(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+
+        // queue : all starting cells with rotten oranges
+        Queue<Integer> queue = new ArrayDeque();//可变数组，线程不安全
+        Map<Integer, Integer> depth = new HashMap();
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c)
+                if (grid[r][c] == 2) {//搜索一遍初始的腐烂橘子
+                    int code = r * C + c;//code
+                    queue.add(code);
+                    depth.put(code, 0);
+                }
+
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int code = queue.remove();
+            int r = code / C, c = code % C;
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 <= nr && nr < R && 0 <= nc && nc < C && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    int ncode = nr * C + nc;
+                    queue.add(ncode);
+                    depth.put(ncode, depth.get(code) + 1);
+                    ans = depth.get(ncode);
+                }
+            }
+        }
+
+        for (int[] row: grid)
+            for (int v: row)
+                if (v == 1)
+                    return -1;
+        return ans;
+
+    }
+}
+
---
+++ src/xyz/zxcwxy999/Offer/No10II/Solution.java
@@ -0,0 +1,13 @@
+package xyz.zxcwxy999.Offer.No10II;
+
+class Solution {
+    public int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for(int i = 0; i < n; i++){
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
